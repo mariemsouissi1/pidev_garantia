01d48d3e8df3d45b6a2c0cab35db6496f708159a
MohamedHachicha <mohamed.hachicha@esprit.tn>
2022-04-03 16:37:11 +0100

    a

--- pidev/src/main/java/services/EmployeeServiceImp.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public class EmployeeServiceImp implements IEmployeeService{
-
-}
--- pidev/src/main/java/tn/esprit/infini2/entities/Claim_type.java
+++
@@ -1,5 +0,0 @@
-package tn.esprit.infini2.entities;
-
-public enum Claim_type {
-Reclamation,Complaint
-}
