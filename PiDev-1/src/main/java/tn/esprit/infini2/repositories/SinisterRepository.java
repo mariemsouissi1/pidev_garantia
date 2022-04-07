8c5d96c9d0660341d3c9a3d24228acefbd950032
BEDISTRIGUI <100165263+BEDISTRIGUI@users.noreply.github.com>
2022-04-07 03:29:31 +0100

    s
    modif

--- PiDev-1/src/main/java/tn/esprit/infini2/repositories/SinisterRepository.java
+++ PiDev-1/src/main/java/tn/esprit/infini2/repositories/SinisterRepository.java
@@ -1,12 +1,39 @@
 package tn.esprit.infini2.repositories;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import tn.esprit.infini2.entities.Provisions;
+import tn.esprit.infini2.entities.Contract;
 import tn.esprit.infini2.entities.Sinister;
 
-@Repository
-public interface SinisterRepository extends CrudRepository<Sinister, Long>{
 
+@Repository
+public interface SinisterRepository extends CrudRepository<Sinister, Integer>{
+
+	@Query("SELECT s FROM Sinister s WHERE s.declarationDate between :from and :to ")
+	List<Sinister> findByDeclarationDateBetween(@Param("from") Date from, @Param("to") Date to);
+	
+	@Query("SELECT s FROM Sinister s WHERE s.sinisterDate between :from and :to ")
+	List<Sinister> findBySinisterDateBetween(@Param("from") Date from, @Param("to") Date to);
+	
+	
+	@Query(value = "SELECT * FROM sinister s WHERE s.contract_id= ?1 " , nativeQuery = true)
+    List<Sinister>findAllByContract(Contract c ) ; 
+	
+
+	
+	
+	
+	 
 }
+		
+		
+		 
+		
+	 
+	
+
--- PiDev-1/src/main/java/tn/esprit/infini2/services/ISinisterService.java
+++ PiDev-1/src/main/java/tn/esprit/infini2/services/ISinisterService.java
@@ -1,17 +1,55 @@
 package tn.esprit.infini2.services;
 
+import java.util.Date;
 import java.util.List;
 
 import tn.esprit.infini2.entities.Sinister;
 
 public interface ISinisterService {
-	List<Sinister> retrieveAllSinisters();
+List<Sinister> retrieveAllSinisters();
+	
+	
+	public Sinister addSinister(Sinister sinister ) ;
+	
+	
+	 String deleteSinister(Integer idSinister);
+	 
+	 
+	 String updateSinister(Sinister sinister , Integer idSinister);
+	 
+	 
+	 Sinister retrieveSinister(Integer id);
+	 
 
-	Sinister addSinister(Sinister c);
-
-	void deleteSinister(Long id);
-
-	Sinister updateSinister(Sinister u);
-
-	Sinister retrieveSinister(Long id);
+	 List<Sinister> retrieveSinisterByEmployee(Long idEmployee);
+	 
+	 
+	 List<Sinister> retrieveSinisterByCustomer(Long idCustomer);
+	 
+	 
+	 List<Sinister> retrieveSinisterByDeclarationDate(Date from , Date to );
+	 
+	 
+	 List<Sinister> retrieveSinisterBySinisterDate(Date from , Date to );
+	 
+     
+ 	public String traiterSinister( Integer idSinister) throws Exception;
+ 	
+ 	
+ 	public String verificationSinisterDelay(Integer idSinister);
+ 	
+ 	
+ 	 String suivreSinistre(Integer idSinister);
+ 	 
+ 	 
+ 	String CalculRapiditéReglementSinistre( Integer idSinister) ;
+ 	
+ 	/*public void affectContractToSinister(Integer idSinister,Long idContrat);*/
+ 	
+ 	
+    
+    
+    
+    
+   
 }
