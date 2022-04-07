8b40c33d2251e819e721ade0b0bc0512eea9ce08
oumaymabichiou <98618686+oumaymabichiou@users.noreply.github.com>
2022-04-07 09:10:31 +0200

    commit

---
+++ PidevGarantiaa/src/main/java/tn/esprit/infini2/entities/EmailMessage.java
@@ -0,0 +1,44 @@
+package tn.esprit.infini2.entities;
+
+public class EmailMessage {
+
+	private String to_address;
+	private String subject;
+	private String body;
+
+	public EmailMessage(String to_address, String subject, String body) {
+		super();
+		this.to_address = to_address;
+		this.subject = subject;
+		this.body = body;
+	}
+
+	public EmailMessage() {
+		super();
+	}
+
+	public String getTo_address() {
+		return to_address;
+	}
+
+	public void setTo_address(String to_address) {
+		this.to_address = to_address;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
+
