87a93527b53ce3338ce30e981753d7582a8c502e
BEDISTRIGUI <100165263+BEDISTRIGUI@users.noreply.github.com>
2022-04-07 03:31:02 +0100

    Update ContractRepository.java
    modifs

--- PiDev-1/src/main/java/tn/esprit/infini2/repositories/ContractRepository.java
+++ PiDev-1/src/main/java/tn/esprit/infini2/repositories/ContractRepository.java
@@ -1,11 +1,30 @@
 package tn.esprit.infini2.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import tn.esprit.infini2.entities.Contract;
+import tn.esprit.infini2.entities.Customer;
+import tn.esprit.infini2.entities.Employee;
 
 @Repository
 public interface ContractRepository extends CrudRepository<Contract, Long>{
+	@Query(value = "SELECT * FROM contract c WHERE c.c_customer_account_id_customer_account= ?1 " , nativeQuery = true)
+	List<Contract> findAllByEmployee(Employee employee) ; 
+	
+	
+	@Query(value = "SELECT * FROM contract c WHERE c.c_customer_account_id_customer_account= ?1 " , nativeQuery = true)
+	List<Contract> findAllByCustomer(Customer customer) ; 
+	
+	
+	
+	@Query("SELECT c FROM Contract c join c.C_customerAccount ca join ca.sinister s  "
+			+ "where s.idSinister= :idSinister")
+	Contract retrieveContract(@Param("idSinister") Integer idSinister);
+
 
 }
