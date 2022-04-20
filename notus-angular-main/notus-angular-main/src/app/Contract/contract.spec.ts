ee2b4be63a161cff29d0d4e758a2021a9a3357ac
hizooo <hind.zouaghi@esprit.tn>
2022-04-20 23:10:11 +0100

    .

---
+++ notus-angular-main/notus-angular-main/src/app/Contract/contract.spec.ts
@@ -0,0 +1,7 @@
+import { Contract } from '../shared/Model/contract';
+
+describe('Contract', () => {
+  it('should create an instance', () => {
+    expect(new Contract()).toBeTruthy();
+  });
+});
---
+++ notus-angular-main/notus-angular-main/src/app/shared/Model/contract.ts
@@ -0,0 +1,9 @@
+export class Contract {
+  iidContract : any;
+  durationContract : any;
+  PrimeContract : any;
+  creationDate : any;
+  expirationDate : any;
+  
+
+}
